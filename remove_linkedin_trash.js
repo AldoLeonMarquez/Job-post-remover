1751d792e2ef06d6d410f7c6a80ef551bdfbb347
AldoLeonMarquez <aldoleon98@gmail.com>
2025-03-25 16:39:34 -0600

    almost fixed

--- remove_linkedin_trash.js
+++ remove_linkedin_trash.js
@@ -6,15 +6,48 @@ var blacklistKeywords = ["Microsoft"];
 
 // The event driven function to remove on scroll
 // Reloaded with navigation of the site
-function scrollDelete() {
+function scrollDelete(a) {
+  var elems = document.getElementsByClassName(a);
+  var cards = [];
+  // Loop through the elems and filter for those containing "Oracle"
+  const listItems = elems[0].querySelectorAll("li");
+
+  listItems.forEach((item) => {
+    var companyName = item.getElementsByClassName(
+      "artdeco-entity-lockup__subtitle",
+    );
+    if (companyName[0]) {
+      if (
+        blacklistKeywords.some((keyword) =>
+          companyName[0].textContent.match(keyword),
+        )
+      ) {
+        cards.push(item);
+      }
+    }
+  });
+
+  // Log the filtered spans only once when scrolled, and onl
+  cards.forEach((span) => {
+    var deleteLmao = span;
+    deleteLmao.remove();
+    var companyName = span.getElementsByClassName(
+      "artdeco-entity-lockup__subtitle",
+    );
+    if (companyName[0]) {
+      console.log(companyName[0].textContent.trim(), "Position Removed");
+    }
+  });
+}
+function scrollDeleteSuggested() {
   var spans = document.getElementsByClassName(
-    "job-card-container__primary-description",
+    "update-components-header__text-view",
   );
   var cards = [];
   // Loop through the spans and filter for those containing "Oracle"
   for (let span of spans) {
     var spanText = span.textContent.trim();
-    if (blacklistKeywords.some((keyword) => spanText.includes(keyword))) {
+    if (spanText == "Suggested") {
       cards.push(span);
     }
   }
@@ -24,7 +57,7 @@ function scrollDelete() {
       span.parentElement.parentElement.parentElement.parentElement.parentElement
         .parentElement;
     deleteLmao.remove();
-    console.log(span.textContent.trim(), "Position Removed");
+    console.log("Removed Ad!");
     // }
   });
 }
@@ -32,23 +65,49 @@ function scrollDelete() {
 function removeTrashPostings(div) {
   // Get the scrollable div, they might change the name which is pretty annoying :/
   if (div) {
-    scrollDiv = document.getElementsByClassName(div);
-    if (scrollDiv[0]) {
-      clearInterval(intervalDiv); // Stop the interval once the div is found
-      // Do something with myDiv
-      scrollDiv[0].addEventListener("scroll", scrollDelete);
+    // Updated to the new name for divs
+    const parentDiv = document.getElementsByClassName("scaffold-layout__list "); // Replace with your parent's class
+    // Loop through the child elements of the parent div
+    const childDivs = parentDiv[0].children;
+    for (let i = 0; i < childDivs.length; i++) {
+      const child = childDivs[i];
+      // Check if the child div is scrollable
+      if (
+        child.scrollHeight > child.clientHeight ||
+        child.scrollWidth > child.clientWidth
+      ) {
+        // Attach the event listener to the scrollable div
+        console.log("adding event listener");
+        clearInterval(intervalDiv);
+        child.addEventListener("scroll", function () {
+          scrollDelete(child.className);
+        });
+      }
     }
+    // scrollDiv = document.getElementsByClassName(div);
+    // if (scrollDiv[0]) {
+    //   clearInterval(intervalDiv); // Stop the interval once the div is found
+    //   // Do something with myDiv
+    //   scrollDiv[0].addEventListener("scroll", scrollDelete);
+    // }
   } else {
     clearInterval(intervalDiv);
     document.addEventListener("scroll", scrollDelete);
   }
 }
 
+function removeSuggestedPosts() {
+  if (document) {
+    clearInterval(intervalDiv);
+    document.addEventListener("scroll", scrollDeleteSuggested);
+  }
+}
+
 // Initial load
 let currentURL = window.location.href;
 if (
-  currentURL.includes("https://www.linkedin.com/jobs/search") ||
-  currentURL.includes("https://www.linkedin.com/jobs/collections")
+  currentURL.match("https://www.linkedin.com/jobs/search") ||
+  currentURL.match("https://www.linkedin.com/jobs/collections")
 ) {
   //Run the thing
   // Attempt to get the div every 250 milliseconds.
@@ -58,35 +117,46 @@ if (
     350,
     "jobs-search-results-list",
   );
-} else if (currentURL.includes("https://www.linkedin.com/jobs")) {
+} else if (currentURL.match("https://www.linkedin.com/jobs")) {
   intervalDiv = setInterval(removeTrashPostings, 350, "");
+} else if (currentURL.match("https://www.linkedin.com/feed/")) {
+  intervalDiv = setInterval(removeSuggestedPosts, 350);
 }
 
 //Start the event listener for navigation
 navigation.addEventListener("navigate", (event) => {
   destination = event.destination.url;
   if (
-    destination.includes("https://www.linkedin.com/jobs/search") ||
-    destination.includes("https://www.linkedin.com/jobs/collections")
+    destination.match("https://www.linkedin.com/jobs/search") ||
+    destination.match("https://www.linkedin.com/jobs/collections")
   ) {
     // Remove the existing  event listener
     if (scrollDiv && scrollDiv[0]) {
       scrollDiv[0].removeEventListener("scroll", scrollDelete);
     }
     document.removeEventListener("scroll", scrollDelete);
+    document.removeEventListener("scroll", scrollDeleteSuggested);
     // Then restart the whole thing
     intervalDiv = setInterval(
       removeTrashPostings,
       350,
-      "jobs-search-results-list",
+      "scaffold-layout__list",
     );
-  } else if (destination.includes("https://www.linkedin.com/jobs")) {
+  } else if (destination.match("https://www.linkedin.com/jobs")) {
     // Remove the existing  event listener
     if (scrollDiv && scrollDiv[0]) {
       scrollDiv[0].removeEventListener("scroll", scrollDelete);
     }
     document.removeEventListener("scroll", scrollDelete);
+    document.removeEventListener("scroll", scrollDeleteSuggested);
     // Then restart the whole thing
     intervalDiv = setInterval(removeTrashPostings, 350, "");
+  } else if (destination.match("https://www.linkedin.com/feed/")) {
+    if (scrollDiv && scrollDiv[0]) {
+      scrollDiv[0].removeEventListener("scroll", scrollDelete);
+    }
+    document.removeEventListener("scroll", scrollDelete);
+    document.removeEventListener("scroll", scrollDeleteSuggested);
+    intervalDiv = setInterval(removeSuggestedPosts, 350);
   }
 });
